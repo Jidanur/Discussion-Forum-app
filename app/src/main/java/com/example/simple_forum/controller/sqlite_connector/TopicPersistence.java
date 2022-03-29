18723d626c81cbea9d3de3735c51c35489355933
Muhammad Faheem Bunally <bunallmf@myumanitoba.ca>
2022-03-29 23:12:25 +0000

    manager database for topic table

---
+++ app/src/main/java/com/example/simple_forum/controller/sqlite_connector/TopicPersistence.java
@@ -0,0 +1,39 @@
+package com.example.simple_forum.controller.sqlite_connector;
+
+import com.example.simple_forum.models.Topic;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TopicPersistence {
+
+    private final IDBManager idbManager;
+
+    public TopicPersistence(IDBManager db) {
+        this.idbManager = db;
+        db.connect();
+        db.initialize();
+    }
+
+    public void add_topic(Topic t){
+        Statement stmt;
+
+        if(idbManager.connect()) {
+            try{
+                stmt  = idbManager.getConnection().createStatement();
+
+                String username  = t.getUser().getUsername();
+
+                String statement = "SELECT id FROM user WHERE username = " + username;
+
+                ResultSet rs = stmt.executeQuery(statement);
+                int user = rs.getInt("id");
+
+                
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
