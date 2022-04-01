acfe7d8eb49d04c7559d8b40a119849780a3a9fa
Muhammad Faheem Bunally <bunallmf@myumanitoba.ca>
2022-04-01 20:08:42 +0000

    Upload New File

---
+++ app/src/main/java/com/example/simple_forum/controller/Main.java
@@ -0,0 +1,42 @@
+package com.example.simple_forum.controller.application;
+
+
+import com.example.simple_forum.controller.managers.TopicManager;
+import com.example.simple_forum.models.Topic;
+import com.example.simple_forum.models.User;
+import java.sql.*;
+
+public class Main {
+
+    private static String dbName="SIMPLE_FORUM_DB.db";
+
+    public static void main(String[] args)
+    {
+        TopicManager topicManager = new TopicManager(true);
+        topicManager.add(new Topic("Movies", new User(), "2022-02-28T00:23:06.035252Z"));
+        topicManager.add(new Topic("Video Games", new User(), "2022-02-28T00:23:06.035252Z"));
+        topicManager.add(new Topic("Cars", new User(), "2022-02-28T00:23:06.035252Z"));
+        topicManager.add(new Topic("Popular", new User(), "2022-02-28T00:23:06.035252Z"));
+        topicManager.add(new Topic("Music", new User(), "2022-02-28T00:23:06.035252Z"));
+
+        System.out.println("All done");
+    }
+
+    public static void setDBPathName(final String name) {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver").newInstance();
+//            Class.forName("org.sqlite.JDBC");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dbName = name;
+    }
+
+    public static String getDBPathName() {
+        return dbName;
+    }
+}
