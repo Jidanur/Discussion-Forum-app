066ae561d6a151c5a70a3a4e7e9ec34213bfd127
Muhammad Faheem Bunally <bunallmf@myumanitoba.ca>
2022-04-01 02:21:21 +0000

    Update app/src/main/java/com/example/simple_forum/controller/sqlite_connector/ITopicPersistence.java

--- app/src/main/java/com/example/simple_forum/controller/sqlite_connector/ITopicPersistence.java
+++ app/src/main/java/com/example/simple_forum/controller/sqlite_connector/ITopicPersistence.java
@@ -1,1 +1,12 @@
+package com.example.simple_forum.controller.sqlite_connector;
+
+import com.example.simple_forum.models.Topic;
+
+import java.util.ArrayList;
+
+public interface ITopicPersistence {
+
+    void add_Topic(Topic t);
+    ArrayList<Topic> get_TopicList(String data);
+}
 
