467eae4b440d9403081cf7448ebc89ef0d367801
JIDAN99 <jidanurrahman99@gmail.com>
2022-04-01 15:42:23 -0500

    added integration test

--- app/src/test/java/com/example/simple_forum/DB_integration_test.java
+++ app/src/test/java/com/example/simple_forum/DB_integration_test.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import com.example.simple_forum.controller.application.Main;
+import com.example.simple_forum.controller.application.Services;
 import com.example.simple_forum.controller.persistence.ITopicPersistence;
 import com.example.simple_forum.controller.persistence.TopicPersistenceHSQLDB;
 import com.example.simple_forum.models.Topic;
@@ -12,59 +14,86 @@ import com.example.simple_forum.models.User;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class DB_integration_test {
 
 
-    String fileName,extension;
-    File tempFile;
+    String fileName;
+    Path tempFile;
     ITopicPersistence db;
+    Topic t;
 
     @Before
     public void init(){
-        try{
-            fileName = "testDB";
-            extension = ".script";
 
-           tempFile = File.createTempFile(fileName,extension);
-
-            db = new TopicPersistenceHSQLDB(tempFile.getPath());
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        finally {
-            System.out.println("worked");
-        }
+        User newUser = new User();
+        fileName = "testDB";
+        Main.setDBPathName(fileName);
+        db = Services.getTopicPersistence();
+        t = new Topic("test",newUser,"date");
 
     }
 
     @After
     public void clear(){
-        try{
-            tempFile.delete();
 
+        Topic get = db.get(t.getTitle());
+        if(get != null){
+            db.delete_topic(t);
         }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+
     }
 
 
     @Test
+    public void test_connection(){
+
+    }
+
+    @Test
     public void test_add(){
-
-        User newUser = new User();
-
-        Topic t = new Topic("test",newUser,"date");
 
         db.insert_topic(t);
 
         ArrayList<Topic> list = db.get_all();
 
         assertEquals(1,list.size());
+
+    }
+
+    @Test
+    public void test_delete(){
+
+        db.insert_topic(t);
+
+        ArrayList<Topic> list = db.get_all();
+
+        assertEquals(1,list.size());
+
+        db.delete_topic(t);
+
+        assertEquals(0,list.size());
+
+    }
+
+    @Test
+    public void test_get(){
+
+        db.insert_topic(t);
+
+        ArrayList<Topic> list = db.get_all();
+
+        assertEquals(1,list.size());
+
+
+        Topic get = db.get(t.getTitle());
+
+        assertNotNull(get);
+
 
     }
 
